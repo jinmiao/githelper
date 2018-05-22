d6dceb8b67fdaae8bd3ef587be67bf023f9436b3
jinmiao ouyang <jinmiao0601@gmail.com>
2018-05-22 23:32:39 +0800

    clean code

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,40 +1,60 @@
-//index.js
 Page({
   data: {
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
-    indicatorDots: true,
-    vertical: false,
-    autoplay: false,
-    circular: false,
-    interval: 2000,
-    duration: 500,
-    previousMargin: 0,
-    nextMargin: 0
+    list: [
+      {
+        id: 'view',
+        name: '创建版本库',
+        open: false,
+        pages: ['git clone <url>', 'git init']
+      }, 
+      {
+        id: 'content',
+        name: '修改和提交',
+        open: false,
+        pages: ['git status', 'git diff', 'git add .', 'git add <file>', 'git mv <old> <new>', 'git add', 'git rm <file>', 'git rm --cached <file>', 'git commit -m "msg"', 'git commit --amend']
+      }, 
+      {
+        id: 'form',
+        name: '查看提交历史',
+        open: false,
+        pages: ['git log', 'git log -p <file>', 'git blame <file>']
+      }, 
+      {
+        id: 'nav',
+        name: '撤销',
+        open: false,
+        pages: ['git checkout [HEAD] <file>','git reset --hard HEAD','git revert <commit>']
+      }, 
+      {
+        id: 'media',
+        name: '分支与标签',
+        open: false,
+        pages: ['git branch', 'git checkout <branch/tag>', 'git branch <newBranch>', 'git branch -d <branch>','git tag','git tag <tagName>','git tag -d <tagName>']
+      }, 
+      {
+        id: 'map',
+        name: '合并和衍合',
+        pages: ['git merge <branch>','git rebase <branch>']
+      }, 
+      {
+        id: 'canvas',
+        name: '远程操作',
+        pages: ['git remote -v','git remote show <branch>','git remote add <remote> <url>','git fetch <remote>','git pull <remote> <branch>','git push <remote> <branch>',"git push <remote> :<branch/tagName>",'git push --tags']
+      }
+    ]
   },
-  changeProperty: function (e) {
-    var propertyName = e.currentTarget.dataset.propertyName
-    var newData = {}
-    newData[propertyName] = e.detail.value
-    this.setData(newData)
-  },
-  changeIndicatorDots: function (e) {
+  kindToggle: function (e) {
+    var id = e.currentTarget.id, list = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
     this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function (e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange: function (e) {
-    this.setData({
-      duration: e.detail.value
-    })
+      list: list
+    });
   }
 })
+
